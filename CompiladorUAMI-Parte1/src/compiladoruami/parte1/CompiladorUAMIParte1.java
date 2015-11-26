b79346f3f3565526f4573370b69947e251400395
Cecy <Cecy@10.1.16.38>
2015-11-26 17:47:16 -0600

    limpieza

--- CompiladorUAMI-Parte1/src/compiladoruami/parte1/CompiladorUAMIParte1.java
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/CompiladorUAMIParte1.java
@@ -13,11 +13,9 @@ package compiladoruami.parte1;
  */
 public class CompiladorUAMIParte1 {
 
-  //hola
+  
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.println("Hola mundo");
     }
     
     
