c0b5cc8327dc14817a26f856926365e1c4386469
Cecy <Cecy@10.1.16.38>
2015-11-26 17:59:08 -0600

    Agregue clase Globales

---
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/Globales.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladoruami.parte1;
+
+/**
+ *
+ * @author Cecy, Lety y Gab
+ */
+public class Globales {
+    public final String PROGRAMA = "programa";
+    public final String COMIENZA = "comienza";
+    public final String TERMINA = "termina";
+    public final String ESCRIBE = "escribe";
+    public final String IMPRIME = "imprime";
+    public final String READ = "read";
+    public final String HECHO = "FIN DE ARCHIVO";
+    public final String EOS = "EOS";
+    //public final String ASIGNACION = "ASIGNACION";
+    public final String ID = "IDENTIFICADOR";
+    public final String NUM_ENT = "ENTERO";
+    public final String CADENA = "CADENA";
+    public final String SI = "si";
+    public final String ENTONCES = "entonces";
+    public final String OTRO = "otro";
+    public final String MIENTRAS = "mientras";
+    public final String HAZ = "haz";
+    public final String DUMP = "dump";
+    public final String HALT = "halt";
+    public final String REPITE = "repìte";
+    public final String HASTA = "hasta";
+    public final String PARA = "para";
+    public final String A = "a";
+    public final String COMENTARIO = "COMENTARIO";
+    public final String RELOP = "OPERADOR RELACIONAL";
+    public final String LOGOP = "OPERADOR LOGICO";
+    public final String LT = "<";
+    public final String LE = "<=";
+    public final String EQ = "==";
+    public final String ASIGNACION = "=";
+    public final String GE = ">=";
+    public final String GT = ">";
+    public final String NE = "!=";
+    public final String NOT = "!";
+    public final String ADDOP = "OPERADOR DE SUMA O RESTA";
+    public final String MAS = "+";
+    public final String MENOS = "-";
+    public final String OR = "||";
+    public final String MULOP = "OPERADOR DE MULTIPLICACION O DIVISION";
+    public final String MULT = "*";
+    public final String DIV = "/";
+    public final String MODULO = "%";
+    public final String AND = "&&";
+    public final String STRINGS = "CADENA DE CARACTERES";
+    public final String ERROR = "ERROR LEXICOGRAFICO";
+    public final String TOKEN_INV = "TOKEN INVALIDO";
+    public final String RESTO_MUNDO = "RESTO DEL MUNDO";
+    public final String error="ERROR SINTACTICO";
+}
