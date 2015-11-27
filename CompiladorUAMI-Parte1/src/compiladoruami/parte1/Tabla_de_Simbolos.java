33bfd7dcfdfc6949a47b38aff32e8d84b539b5f9
Zempoalteca <Zempoalteca@10.1.42.202>
2015-11-26 18:26:59 -0600

    Cambie todo el codigo por el mio

--- CompiladorUAMI-Parte1/src/compiladoruami/parte1/Tabla_de_Simbolos.java
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/Tabla_de_Simbolos.java
@@ -7,7 +7,7 @@ package compiladoruami.parte1;
 
 /**
  *
- * @author Cecy, Lety y Gab
+ * @author Gabriel Zempoalteca Garrido
  */
 public class Tabla_de_Simbolos {
 
@@ -18,12 +18,11 @@ public class Tabla_de_Simbolos {
     public Tabla_de_Simbolos() {
         ultimo_entrar = 0;
         TABLA_DE_SIMBOLOS = new String[SYMMAX][2];
-//
-        //
+
     }
 
     public int Buscar_Simbolo(String Lexema) {
-        int i=0;
+        int i;
         for(i=ultimo_entrar; i>=0; i--){
             if(Lexema.equals(TABLA_DE_SIMBOLOS[i][0])){
                 return i;
@@ -33,18 +32,17 @@ public class Tabla_de_Simbolos {
     }
 
     public String Obtener_Token(int i) {
-        return TABLA_DE_SIMBOLOS[i][1];
+        return TABLA_DE_SIMBOLOS[i][1];          
 
     }
 
     public String Obtener_Lexema(int i) {
-        return TABLA_DE_SIMBOLOS[i][0];
+        return TABLA_DE_SIMBOLOS[i][0];          
 
     }
 
     public int Inserta_en_Tabla_Simbolos(String Lexema, String Token) {
         if (ultimo_entrar < SYMMAX) {
-
             TABLA_DE_SIMBOLOS[ultimo_entrar][0] = Lexema;
             TABLA_DE_SIMBOLOS[ultimo_entrar][1] = Token;
             ultimo_entrar++;
@@ -58,12 +56,12 @@ public class Tabla_de_Simbolos {
     public void Imprimir_Tabla() {
        int i;
         for(i=0; i<ultimo_entrar; i++){
-            UAMI.wr1.append(+i+" " +" ["+TABLA_DE_SIMBOLOS[i][0]+ "]\t["+TABLA_DE_SIMBOLOS[i][1]+"]\n");
+            UAMI.wr1.append("["+TABLA_DE_SIMBOLOS[i][0]+ "]\t["+TABLA_DE_SIMBOLOS[i][1]+"]\n");
         }
     }
 
     public void Inicializa_Palabras_Reservadas() {
-        TABLA_DE_SIMBOLOS[0][0] = "programa";//
+        TABLA_DE_SIMBOLOS[0][0] = "programa";       
         TABLA_DE_SIMBOLOS[1][0] = "comienza";
         TABLA_DE_SIMBOLOS[2][0] = "termina";
         TABLA_DE_SIMBOLOS[3][0] = "escribe";
