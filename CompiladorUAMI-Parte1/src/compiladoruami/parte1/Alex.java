686472d9fbc620322158e9b5535d69b71ff951e9
Zempoalteca <Zempoalteca@10.1.42.202>
2015-11-26 18:17:04 -0600

    Cambie paquete

--- CompiladorUAMI-Parte1/src/compiladoruami/parte1/Alex.java
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/Alex.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package PARSER_V1;
+package compiladoruami.parte1;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
