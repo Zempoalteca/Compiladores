1d42b2974d50bfe102de1032726c59c9cf47b40a
Leticia <Leticia@10.1.28.109>
2015-12-02 12:03:10 -0600

    Creacion de la clase Generador_Codigo_Intermedio

---
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/Generador_Codigo_Intermedio.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compiladoruami.parte1;
+
+/**
+ *
+ * @author Leticia
+ */
+public class Generador_Codigo_Intermedio {
+ 
+    public void Emite(String Valor_Token,String Lexema ){
+
+}
+    
+}
+
+
