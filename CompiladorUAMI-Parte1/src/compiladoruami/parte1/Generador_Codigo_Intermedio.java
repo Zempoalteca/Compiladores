d1af0ef64b475524dc648007921670a5087d0f79
Zempoalteca <Zempoalteca@10.1.42.202>
2015-12-04 17:40:35 -0600

    Revision1

--- CompiladorUAMI-Parte1/src/compiladoruami/parte1/Generador_Codigo_Intermedio.java
+++ CompiladorUAMI-Parte1/src/compiladoruami/parte1/Generador_Codigo_Intermedio.java
@@ -20,33 +20,33 @@ public class Generador_Codigo_Intermedio {
                 UAMI.wr3.append(G1.HALT + "\n");
                 break;
             case "lvalue":
-                UAMI.wr3.append(G1.VALOR_I + " " + Lexema);
+                UAMI.wr3.append(G1.VALOR_I + " " + Lexema + "\n");
                 break;
             case "rvalue":
-                UAMI.wr3.append(G1.VALOR_D + " " + Lexema);
+                UAMI.wr3.append(G1.VALOR_D + " " + Lexema + "\n");
                 break;
             case "label":
-                UAMI.wr3.append(G1.ETIQUETA + " " + Lexema);
+                UAMI.wr3.append(G1.ETIQUETA + " " + Lexema + "\n");
                 break;
             case "push":
-                UAMI.wr3.append(G1.PUSH + " " + Lexema);
+                UAMI.wr3.append(G1.PUSH + " " + Lexema + "\n");
                 break;
             case "gofalse":
-                UAMI.wr3.append(G1.SI_FALSO_VE_A + " " + Lexema);
+                UAMI.wr3.append(G1.SI_FALSO_VE_A + " " + Lexema + "\n");
                 break;
             case "goto":
-                UAMI.wr3.append(G1.VE_A + " " + Lexema);
+                UAMI.wr3.append(G1.VE_A + " " + Lexema + "\n");
                 break;
             case "write":
                 UAMI.wr3.append(G1.ELIMINA + "\n");
                 break;
             case "print":
-                UAMI.wr3.append(G1.COPIA + " " + Lexema);
+                UAMI.wr3.append(G1.COPIA + " " + Lexema + "\n");
                 break;
             case ":=":
-                UAMI.wr3.append(G1.ASIGN + " " + Lexema);
+                UAMI.wr3.append(G1.ASIGN + " " + Lexema + "\n");
                 break;
-            default:
+            //default:
             case "OPERADOR RELACIONAL":
             case "OPERADOR LOGICO":
             case "OPERADOR DE SUMA O RESTA":
